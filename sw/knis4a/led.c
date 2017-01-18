e1f970845ea873ff47f66478c1b651f00b0d0953
Bert Outtier <outtierbert@gmail.com>
2017-01-18 21:19:56 +0100

    [LED] refactoring setting leds

--- sw/knis4a/led.c
+++ sw/knis4a/led.c
@@ -26,6 +26,20 @@
 #include <libopencm3/stm32/gpio.h>
 #include "led.h"
 
+typedef struct {
+  uint32_t  	gpioport;
+	uint16_t  	gpios;
+} led_gpio;
+
+const led_gpio leds[LEDS_COUNT]={
+  { .gpioport = GPIOC, .gpios = GPIO13 },
+  { .gpioport = GPIOB, .gpios = GPIO3 },
+  { .gpioport = GPIOA, .gpios = GPIO3 },
+  { .gpioport = GPIOB, .gpios = GPIO0 },
+  { .gpioport = GPIOA, .gpios = GPIO2 },
+  { .gpioport = GPIOA, .gpios = GPIO5 }
+};
+
 void led_setup(void)
 {
 	rcc_periph_clock_enable(RCC_GPIOA);
@@ -35,10 +49,49 @@ void led_setup(void)
 	gpio_mode_setup(GPIOB, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO0 | GPIO3);
   gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO13);
 
-  gpio_clear(GPIOC,GPIO13); // LED_A1
-  gpio_set(GPIOB,GPIO3);    // LED_A2
-  gpio_clear(GPIOA,GPIO3);  // LED_A3
-  gpio_clear(GPIOB,GPIO0);  // LED_A4
-  gpio_clear(GPIOA,GPIO2);  // LED_A5
-  gpio_clear(GPIOA,GPIO5);  // LED_A6
+  set_led(LED_A2);
 }
+
+void set_led(e_leds led){
+  int i;
+  for(i=0; i<LEDS_COUNT; i++) {
+    gpio_clear(leds[i].gpioport, leds[i].gpios);
+  }
+  gpio_set(leds[led].gpioport, leds[led].gpios);
+}
+
+// void tim6_isr(void)
+// {
+// 	TIM_SR(TIM6) &= ~TIM_SR_UIF;
+//   currentLed++;
+//   currentLed%=MAX_LEDS;
+//   for(i=0; i< MAX_LEDS; i++)
+//   {
+//     if(i == currentLed) gpio_set(GPIOB,GPIO3);
+//   }
+//
+// 	gpio_toggle(GPIOC,GPIO13);
+//   gpio_toggle(GPIOB,GPIO3);    // LED_A2
+//   gpio_toggle(GPIOA,GPIO3);  // LED_A3
+//   gpio_toggle(GPIOB,GPIO0);  // LED_A4
+//   gpio_toggle(GPIOA,GPIO2);  // LED_A5
+//   gpio_toggle(GPIOA,GPIO5);  // LED_A6
+// }
+//
+// /*
+//  * setup timer 6 to generate an overflow interrupt at 1ms
+//  * It is used to switch leds
+//  */
+// void led_setup(void)
+// {
+// 	rcc_periph_reset_pulse(RST_TIM6);
+// 	/* 48Mhz / 10khz -1. */
+// 	timer_set_prescaler(TIM6, 4799); /* 48Mhz/10000hz - 1 */
+// 	/* 10khz for 10 ticks = 1 khz overflow = 1ms overflow interrupts */
+// 	timer_set_period(TIM6, 10);
+//
+// 	nvic_enable_irq(NVIC_TIM6_IRQ);
+// 	timer_enable_update_event(TIM6); /* default at reset! */
+// 	timer_enable_irq(TIM6, TIM_DIER_UIE);
+// 	timer_enable_counter(TIM6);
+// }
--- sw/knis4a/led.h
+++ sw/knis4a/led.h
@@ -25,9 +25,20 @@
 #ifndef LED_H_
 #define LED_H_
 
+typedef enum {
+  LED_A1,
+  LED_A2,
+  LED_A3,
+  LED_A4,
+  LED_A5,
+  LED_A6,
+  LEDS_COUNT
+} e_leds;
+
 /**
  * Initialize and start the PWM used for the LEDs.
  */
 void led_setup(void);
+void set_led(e_leds led);
 
 #endif
